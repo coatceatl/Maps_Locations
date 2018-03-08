57c0ed5940ebe6a9120cc978a844ec52404df4f1
Oksana Tkachenko <oksana@crystax.net>
2018-03-08 19:51:00 +0200

    adds marker and infoWindow

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ function initMap() {
     center: {lat: 32.2750268, lng: 34.8458601 },
     zoom: 12
   });
+  var opera = { lat: 32.0762634, lng: 34.7849464 };
+  var marker = new google.maps.Marker({
+    position: opera,
+    map: map,
+    title: 'The Opera House'
+  });
+  var infoWindow = new google.maps.InfoWindow({
+    content: 'The Opera House'
+  });
+  marker.addListener('click', function() {
+    infoWindow.open(map, marker);
+  });
 }
 
 google.maps.event.addListener(initMap);
