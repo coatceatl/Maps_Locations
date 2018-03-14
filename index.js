476992541c0d491da8a7210f3d6cf538904b3756
Oksana Tkachenko <oksana@crystax.net>
2018-03-14 22:57:11 +0200

    adds list of locations

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ function initMap() {
     center: {lat: 32.2750268, lng: 34.8458601 },
     zoom: 12
   });
-  var opera = { lat: 32.0762634, lng: 34.7849464 };
-  var marker = new google.maps.Marker({
-    position: opera,
-    map: map,
-    title: 'The Opera House'
-  });
+  var locations = [
+    {title: 'Hayarkon Park', location: {lat: 32.0992549, lng: 34.8132114}},
+    {title: 'Raanana Park Amphitheater', location: {lat: 32.188737, lng: 34.8527066}},
+    {title: 'Caesaria National Park', location: {lat: 32.5086606, lng: 34.9001683}},
+    {title: 'Irus Argaman Reserve', location: {lat: 32.283054, lng: 34.839627}},
+    {title: 'Carmel Mountain National Park', location: {lat: 32.7290981, lng:34.9992672}},
+    {title: 'Bahai Gardens', location: {lat: 32.8119338, lng: 34.9866286}}
+  ];
+  /*
   var infoWindow = new google.maps.InfoWindow({
-    content: 'The Opera House'
+    content: 'This is a Irus Argaman Reserve'
   });
   marker.addListener('click', function() {
     infoWindow.open(map, marker);
   });
+  */
 }
 
 google.maps.event.addListener(initMap);
